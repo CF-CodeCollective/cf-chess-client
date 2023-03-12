44fe165620d6944e0fcf1e3494f9ed33ddef48cf
John Harris <vosm803@gmail.com>
2023-03-12 14:03:04 -0400

    Added connection_handler.py

---
+++ src/connection/connection_handler.py
@@ -0,0 +1,8 @@
+'''
+To avoid any issues with security concerns IP and port to listen to will be in a separate text file in the
+.gitignore, to get the info, just ask.
+'''
+
+class ConnectionHandler():
+    def start_connection(self):
+        print("Hello World!")
