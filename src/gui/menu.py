3fe8d489f731f66a66d3dd2443865c980cb442c5
Brandon Scott Floyd <bfloyd@beaconss.com>
2023-04-12 10:43:47 -0400

    I created a seperate Menu.PY file with PyQt5 to start a basic menu I kept it seperate from the main file to make sure this is what we wanted in the first place

---
+++ src/gui/menu.py
@@ -0,0 +1,47 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+
+
+class Window(QMainWindow):
+    """Main Window."""
+
+    def createMenuBar(self):
+        # Added Buttons here to try and make a more complete screen with options that could be used in the future
+        menuBar = self.menuBar()
+        logInMenu = menuBar.addMenu("&Log In")
+        helpMenu = menuBar.addMenu("&Help")
+
+    def UiComponents(self):
+        # creating a push button
+        button = QPushButton("Click To Play", self)
+        # setting geometry of button
+        button.setGeometry(200, 150, 100, 30)
+        # adding action to a button
+        button.clicked.connect(self.clickAction)
+
+    def clickAction(self):
+        # printing pressed
+        # Need to add link to start main game pointing to a print statement till I find a way to launch. -BF
+        print("Game Started")
+
+    # Launches Menu screen pointed at self
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent)
+        self.setWindowTitle("Code Collective Chess Team")
+        self.resize(800, 400)
+        self.centralWidget = QLabel("Welcome to The Chess Project!")
+        self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.setCentralWidget(self.centralWidget)
+        self.createMenuBar()
+        self.UiComponents()
+
+
+# Creates the instance of the window and menu.
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
